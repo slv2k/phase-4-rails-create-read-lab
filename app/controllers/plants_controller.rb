a83d41585084c7bed452cdb7d66a6f5c7a861310
samuel <samuelmmanor@gmail.com>
2022-03-14 13:50:58 -0400

    commit tree 5a613ed789fece15e422e071db05fb5c1146afab

---
+++ app/controllers/plants_controller.rb
@@ -0,0 +1,30 @@
+class PlantsController < ApplicationController
+
+    # GET /plants
+    def index
+        plants = Plant.all
+        render json: plants
+    end
+
+    # GET /plants/:id
+    def show
+        plant = Plant.find_by(id: params[:id])
+        if plant
+            render json: plant
+        else
+            render json: { error: "Plant not found" }, status: :not_Found
+        end
+    end
+
+    def create
+        plant = Plant.create(plant_params)
+        render json: plant, status: :created
+    end
+
+    private
+    
+    def plant_params
+        params.permit(:name, :image, :price)
+    end
+
+end
